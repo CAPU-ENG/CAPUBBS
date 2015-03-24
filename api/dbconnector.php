ca3f4aab48f3d9c00f5e6cb6a3157be568c9961b
Jack Ji <jiyuanyi1992@gmail.com>
2015-03-24 10:03:56 +0800

    sql connect bug

--- api/dbconnector.php
+++ api/dbconnector.php
@@ -1,6 +1,6 @@
 <?php
 function dbconnect() {
-	$con = @mysql_connect('localhost','root','199521025') 
+	$con = @mysql_connect('localhost','root','19951025') 
 		or die("Cannot connect to database !!!");
 	mysql_query("SET NAMES 'UTF8'");
 }
