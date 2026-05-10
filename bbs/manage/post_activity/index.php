8a9eaf88248f37fb72db9b91c72b6d96ce57f7ef
zhanghaodan.2024 <zhanghaodan.2024@bytedance.com>
2026-05-11 02:43:25 +0800

    feat: 添加报名帖发布页面功能
    实现报名帖发布页面，包括用户登录状态检查、版块选择、标题输入、富文本编辑、附件上传和管理功能
    TODO: 实现问卷发布功能
    
    Change-Id: I435b41b880266f0b9fe1cf04a77b1eca74b1b5fe

---
+++ bbs/manage/post_activity/index.php
@@ -0,0 +1,344 @@
+<?php
+	include("../../lib/mainfunc.php");
+	include "../../../config.php";
+	date_default_timezone_set('Asia/Shanghai');
+	$users=getuser();
+	$username=$users['username'];
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="apple-itunes-app" content="app-id=826386033">
+<title>报名帖发布</title>
+<script type="text/javascript" src="../../lib/general.js"></script>
+<script src="../../lib/jquery.min.js"></script>
+<link rel="stylesheet" href="../../lib/general.css">
+<link rel="stylesheet" href="../../main/style.css">
+<link rel="shortcut icon" href="/assets/images/capu.jpg">
+</head>
+<body>
+<div class="header">
+<br>
+
+<div class="user">
+<?php
+$nowurl=$_SERVER["PHP_SELF"]. "?".$_SERVER["QUERY_STRING"];
+$nowurl=urlencode($nowurl);
+if($username!=""){
+	$userinfo=mainfunc(array("view"=>$username));
+	$userinfo=$userinfo[0];
+	$msg=intval($userinfo['newmsg']);
+	$icon=translateicon($userinfo['icon']);
+	$star=intval($userinfo['star']);
+	echo("<img src='$icon' class='usericon'></img>");
+	echo("<div class='userinfo'>");
+	echo("<a href='/bbs/user/?name=$username' target='_blank'>$username</a>");
+	echo("&nbsp;等级：$star");
+	
+	echo("<script type='text/javascript'>");
+	echo("var score=".$userinfo['score'].";");
+	echo("var star=".$star.";");
+	echo("</script>");
+	
+	if($msg==0){
+		echo("&nbsp;<a href='/bbs/home' target='_blank'>个人中心</a>");
+	}else{
+		echo("<br><a href='/bbs/home?pos=message' target='_blank'>您有 $msg 条未读消息</a>");
+	}
+	echo("<br><a href='/bbs/logout?from=$nowurl'>注销</a>");
+	echo("</div>");
+}else{
+	echo("<script type='text/javascript'>var score=-1;</script>");
+	echo("<span class='guest'>欢迎您，游客！<a href='/bbs/login?from=$nowurl'>登录</a> 或者 <a href='/bbs/register'>注册</a></span>");
+}
+?>
+
+</div>
+
+</div>
+<br>
+<?php
+    if ($username!="") {
+?>
+		<div class="editor" id="editor">
+            <div>
+                选择版块：
+                <select name="bid" id="fm_bid" style="min-width:180px;">
+                    <option value="1">车协工作区</option>
+                    <option value="2">行者足音</option>
+                    <option value="3">车友宝典</option>
+                    <option value="4">纯净水</option>
+                    <option value="5">考察与社会</option>
+                    <option value="6">五湖四海</option>
+                    <option value="7">一技之长</option>
+                    <option value="9">竞赛竞技</option>
+                    <option value="28">网站维护</option>
+                </select>
+            </div>
+            <br>
+			<input type="text" class="title" placeholder="帖子标题" id="raw_title">
+			<div id="edi_bar"></div>
+			<div id="edi_content" onfocus="editorFocus();" onblur="editorBlur();"></div>
+			<br>
+			<progress max="100" value="20" id="progress"></progress>
+			<div id="edi_attach" onclick="attach();">添加附件</div>
+			<input type="file" id="file" style="display:none;" onchange="fileselected();">
+			选择签名档：
+			<input type="radio" name="sign" value="0">不使用
+			<input type="radio" name="sign" value="1" checked>1
+			<input type="radio" name="sign" value="2">2
+			<input type="radio" name="sign" value="3">3
+
+			<div id="edi_submit" onclick="doreply();">发表帖子</div>
+			<br><br><br>
+			<span id="attachtip" style="display:none;">本帖包含的附件：</span>
+			<div class="attachs" id="attachs">
+			
+			</div>
+			<span id="unusedattachtip" style="display:none;">您曾上传但未使用的附件：（可直接链接到本贴）<img src="/bbs/main/waiting.gif" width="15px" id="waitinggif" style="visibility:hidden;"></span>
+			<div class="attachs" id="unusedattachs">
+			
+			</div>
+		</div>
+<?php
+    }
+        else 
+            echo '
+		<div class="editip" id="editip">
+		<span class="editip">您需要&nbsp;<a href="../../login?from='.$nowurl.'">登录</a>&nbsp;后才能发表主题；没有账号？&nbsp;<a href="/bbs/register">现在注册</a>&nbsp;</span>
+		</div>
+';
+?>
+<div class="footer">
+</div>
+
+<script type="text/javascript" src="../../lib/nic.js"></script>
+<script type="text/javascript">
+var myNicEditor = new nicEditor({fullPanel : true});
+myNicEditor.setPanel('edi_bar');
+myNicEditor.addInstance('edi_content');
+var attachs=[];
+var unusedattachs=[];
+<?php
+if ($username!="") {
+    $result=mainfunc(array("ask"=>"unusedattachinfo"));
+    for($i=1;$i< count($result);$i++){
+        echo("unusedattachs.push({
+        name:'".$result[$i]['name']."',
+        size:'".$result[$i]['size']."',
+        price:'".$result[$i]['price']."',
+        id:'".$result[$i]['id']."',
+        auth:'".$result[$i]['auth']."'
+        });\n");
+    }
+}
+?>
+refreshAttach();
+function refreshAttach(){
+	if(attachs.length==0){
+		$('#attachtip,#attachs').hide();
+	}else{
+		$('#attachtip,#attachs').show();
+	}
+	if(unusedattachs.length==0){
+		$('#unusedattachtip,#unusedattachs').hide();
+	}else{
+		$('#unusedattachtip,#unusedattachs').show();
+	}
+	var s="";
+	for(var i=0;i<attachs.length;i++){
+		var a=attachs[i];
+		s+=generateattach(a['name'],a['size'],a['id'],false);
+	}
+	$('#attachs').html(s);
+	var s2="";
+	for(var i=0;i<unusedattachs.length;i++){
+		var a=unusedattachs[i];
+		s2+=generateattach(a['name'],a['size'],a['id'],true);
+	}
+	$('#unusedattachs').html(s2);
+}
+function attach(){
+	$('#file').click();
+}
+function fileselected(){
+	if ($('#file').val()) {
+		priceok();
+	}
+}
+function appendattach(id){
+	for(var i=0;i<unusedattachs.length;i++){
+		if(unusedattachs[i]['id']==id){
+			attachs.push(unusedattachs[i]);
+			unusedattachs.splice(i,1);
+			break;
+		}
+	}
+	refreshAttach();
+}
+function removeattach(id){
+	for(var i=0;i<attachs.length;i++){
+		if(attachs[i]['id']==id){
+			unusedattachs.push(attachs[i]);
+			attachs.splice(i,1);
+			break;
+		}
+	}
+	refreshAttach();	
+}
+function delattach(id){
+	if(confirm("您确定要彻底删除此附件么？")){
+		$('#waitinggif').show();
+		$.post("/bbs/delattach/",{id:id},function(r) {
+			var result=JSON.parse(r);
+			if(result.code==0){
+				for(var i=0;i<unusedattachs.length;i++){
+					if(unusedattachs[i]['id']==id){
+						unusedattachs.splice(i,1);
+						break;
+					}
+				}
+			$('#waitinggif').hide();
+			refreshAttach();
+			}else{
+				alert(result.msg);
+			}
+		});		
+	}
+}
+function generateattach(filename,size,aid,useforappend){
+	var extension=filename.slice(filename.lastIndexOf(".")+1);
+	var supportedExt="bmp csv gif html jpg jpeg key mov mp3 mp4 numbers pages pdf png rtf tiff txt zip ipa ipsw doc docx ppt pptx xls avi wmv mkv mts".split(" ");
+	var imgsrc="file";
+	if(supportedExt.indexOf(extension)!=-1){
+		imgsrc=extension;
+	}
+	imgsrc="/bbs/assets/fileicons/"+imgsrc+".png";
+	var s='<div class="attach">';
+	s+='<img src="'+imgsrc+'" class="fileicon">';
+	s+='<div class="fileinfo"><span class="filename">'+filename+'<br></span>';
+	s+='<span class="sub">'+packSize(size)+'<br>';
+	if(useforappend){
+		s+='<a href="javascript:appendattach('+aid+');">引用</a>&nbsp;&nbsp;';
+		s+='<a href="javascript:delattach('+aid+');">彻底删除</a>';
+	}else{
+		s+='<a href="javascript:removeattach('+aid+');">删除</a>';
+	}
+	s+='</div></div>';
+	return s;
+}
+function packSize(size){
+	if(size<1024) return size+"字节";
+	if(size<1024*1024) return (size/1024).toFixed(1)+"KB";
+	if(size<1024*1024*1024) return (size/1024/1024).toFixed(1)+"MB";
+	return (size/1024/1024/1024).toFixed(1)+"GB";
+}
+
+function onprogress(evt){
+    var prob=document.getElementById("progress");
+    if(prob.style.visibility!="visible") prob.style.visibility="visible";
+    prob.value=evt.loaded;
+    prob.max=evt.total;
+    prob.label=(evt.loaded/evt.total*100).toFixed(1)+"%";
+}
+
+function priceok(){
+	var price=0;
+	var auth=0;
+	var fileObj=document.getElementById("file").files[0];
+	var FileController = "/bbs/attach/";
+	var form = new FormData();
+    form.append("auth", auth);
+    form.append("price", price);
+    form.append("file", fileObj);
+    var xhr = new XMLHttpRequest();
+	xhr.open("post", FileController, true);
+	xhr.onload = function () {
+		var prob=document.getElementById("progress");
+		if(prob.style.visibility!="hidden") prob.style.visibility="hidden";
+		alert("response:"+xhr.responseText+" code:"+xhr.status);
+		try{
+			var result=JSON.parse(xhr.responseText);
+			if(result.code==0){
+				attachs.push({name:fileObj.name,size:fileObj.size,price:price,id:result.msg});
+				refreshAttach();
+			}else{
+				alert("附件上传失败："+result.msg+" code:"+result.code);
+			}
+		}catch(e){
+			alert("出bug了");
+		}
+	};
+	xhr.upload.addEventListener("progress", onprogress, false);
+	xhr.send(form);
+}
+
+function doreply(){
+	if(document.getElementById("raw_title").value.length==0){
+		alert("请填写帖子标题！");
+		return;
+	}
+	var content=$('#edi_content').html();
+	if(content=="<br>" || content=="<div></div>" || content == editorPlaceholder){
+		alert("请填写帖子内容！");
+		return;
+	}
+	var token=getcookie("token");
+	if(!token){
+		alert("尚未登录！");
+		return;
+	}
+	if (content.length > 100000) {
+		alert("内容字符数为"+content.length+"（超过10万字符），请检查是否粘贴了图片。");
+		return;
+	}
+	content=content.replace(/&/g, "&amp;");
+	var bts=document.getElementsByName("sign");
+	var sig = "1";
+	for(var i=0;i<bts.length;i++){
+		if(bts[i].checked){
+			sig=bts[i].value;
+		}
+	}
+	var s="";
+	for(var i=0;i<attachs.length;i++){
+		s+=attachs[i]['id']+" ";
+	}
+	if(s) s=s.slice(0,s.length-1);
+	$.post("/bbs/post/",{
+		bid:$('#fm_bid').val(),
+		tid:-1,
+		token:token,
+		title:$('#raw_title').val(),
+		text:content,
+		sig:sig,
+		attachs:s
+		},function(data) {
+			var x=parseInt(data);
+			if (x==0) {window.location=window.location.href;}
+			else alert("错误："+data);
+		}
+	);
+
+}
+
+const editorPlaceholder = '<div style="color: rgb(118, 118, 118);">如需上传图片请使用右上角的“上传图片”功能，不要将图片直接粘贴在文本框中</div>';
+myNicEditor.instanceById('edi_content').setContent(editorPlaceholder);
+function editorFocus() {
+	if (myNicEditor.instanceById('edi_content').getContent() == editorPlaceholder) {
+		myNicEditor.instanceById('edi_content').setContent('<br>');
+	}
+}
+
+function editorBlur() {
+	let newText = myNicEditor.instanceById('edi_content').getContent();
+	if (newText == '' || newText == '<br>') {
+		myNicEditor.instanceById('edi_content').setContent(editorPlaceholder);
+	}
+}
+
+
+</script>
+</body>
+
+</html>
