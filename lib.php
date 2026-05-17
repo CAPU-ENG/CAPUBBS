dc777b4716b2c566adcd21138114743cc46d77bb
zhanghaodan.2024 <zhanghaodan.2024@bytedance.com>
2026-05-17 15:34:50 +0800

    refactor(lib.php): 移除旧mysql数据库连接函数并优化代码格式
    1. 删除了废弃的mysql扩展dbconnect函数
    2. 统一代码缩进、空格等格式规范
    3. 完善dbconnect_mysqli的注释和代码可读性

--- lib.php
+++ lib.php
@@ -4,33 +4,21 @@
  */
 require_once __DIR__ . '/config.php';
 
-// Database connector.
-function dbconnect() {
-    $con = @mysql_connect(CAPUBBS_DB_HOSTNAME, CAPUBBS_DB_USERNAME,
-        CAPUBBS_DB_PASSWORD) or die("Cannot connect to database !!!");
-
-    // Set to `utf8mb4` in order to support emoji
-	mysql_query("SET NAMES 'utf8mb4'");
-
-	// Allow insert null while the column is defined with not null
-    mysql_query("SET sql_mode = ''");
-}
-
-
+// Database connector (mysqli).
 function dbconnect_mysqli() {
     $con = @mysqli_connect(CAPUBBS_DB_HOSTNAME, CAPUBBS_DB_USERNAME,
         CAPUBBS_DB_PASSWORD, "capubbs") or die("Cannot connect to database !!!");
-    if (mysqli_connect_errno()) { 
+    if (mysqli_connect_errno()) {
         echo "连接 MySQL 失败: " . mysqli_connect_error();
         return null;
-    } 
+    }
 
     // Set to `utf8mb4` in order to support emoji
     mysqli_query($con, "SET NAMES 'utf8mb4'");
 
-	// Allow insert null while the column is defined with not null
+    // Allow insert null while the column is defined with not null
     mysqli_query($con, "SET sql_mode = ''");
-    
+
     return $con;
 }
 
@@ -48,25 +36,25 @@ function checkuser() {
 }
 
 function checkuser_mysqli() {
-    $token=@$_COOKIE['token'];
-    if ($token=="") return array("",0);
+    $token = @$_COOKIE['token'];
+    if ($token == "") return array("", 0);
     $con = dbconnect_mysqli();
     $time = time();
-    $statement="select username, rights from capubbs.userinfo where token='$token' && $time-tokentime<=60*60*24*7";
-    $results=mysqli_query($con, $statement);
-    if (mysqli_num_rows($results)==0) return array("",0);
-    $res=mysqli_fetch_array($results);
+    $statement = "select username, rights from capubbs.userinfo where token='$token' && $time-tokentime<=60*60*24*7";
+    $results = mysqli_query($con, $statement);
+    if (mysqli_num_rows($results) == 0) return array("", 0);
+    $res = mysqli_fetch_array($results);
     return $res;
 }
 
 function checkuser_con($con) {
-    $token=@$_COOKIE['token'];
-    if ($token=="") return array("",0);
+    $token = @$_COOKIE['token'];
+    if ($token == "") return array("", 0);
     $time = time();
-    $statement="select username, rights from capubbs.userinfo where token='$token' && $time-tokentime<=60*60*24*7";
-    $results=mysqli_query($con, $statement);
-    if (mysqli_num_rows($results)==0) return array("",0);
-    $res=mysqli_fetch_array($results);
+    $statement = "select username, rights from capubbs.userinfo where token='$token' && $time-tokentime<=60*60*24*7";
+    $results = mysqli_query($con, $statement);
+    if (mysqli_num_rows($results) == 0) return array("", 0);
+    $res = mysqli_fetch_array($results);
     return $res;
 }
 ?>
