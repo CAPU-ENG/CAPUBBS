2bc8baef8f55168b1940cfb7e1de67dc1195e2ce
zhanghaodan.2024 <zhanghaodan.2024@bytedance.com>
2026-05-17 19:10:40 +0800

    chore: 删除废弃的checkuser旧函数
    移除了已被checkuser_mysqli替代的旧用户验证函数，清理冗余代码

--- lib.php
+++ lib.php
@@ -22,19 +22,6 @@ function dbconnect_mysqli() {
     return $con;
 }
 
-// Check user.
-function checkuser() {
-    $token=@$_COOKIE['token'];
-    if ($token=="") return array("",0);
-    dbconnect();
-    $time=time();
-    $statement="select username, rights from capubbs.userinfo where token='$token' && $time-tokentime<=60*60*24*7";
-    $results=mysql_query($statement);
-    if (mysql_num_rows($results)==0) return array("",0);
-    $res=mysql_fetch_row($results);
-    return $res;
-}
-
 function checkuser_mysqli() {
     $token = @$_COOKIE['token'];
     if ($token == "") return array("", 0);
