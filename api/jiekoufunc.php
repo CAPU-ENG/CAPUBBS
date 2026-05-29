8f7b1e5aa4882a419b6538ce605eed2eae2bba5f
zhanghaodan.2024 <zhanghaodan.2024@bytedance.com>
2026-05-29 20:17:46 +0800

    refactor(api): 将纯工具函数提取到 api/lib/helpers.php
    从 jiekoufunc.php 中提取 6 个无副作用、无 DB 依赖的纯函数：
    jiekoufunc_trans, jiekoufunc_sanitize_xml, jiekoufunc_islegal,
    jiekoufunc_packBool, jiekoufunc_comp, jiekoufunc_getdays
    
    jiekoufunc.php: 2881 → 2838 行
    
    (cherry picked from commit fe3dcb73ff691716a1d4cd9cd7e71e20676e2efb)

--- api/jiekoufunc.php
+++ api/jiekoufunc.php
@@ -10,54 +10,11 @@
  */
 
 require_once __DIR__.'/../lib.php';
+require_once __DIR__.'/lib/helpers.php';
 
 $GLOBALS['validtime']  = 60 * 60 * 24 * 7;   // 7 days
 $GLOBALS['attachroot'] = __DIR__ . "/../bbs/attachment/";
 $GLOBALS['_jiekoufunc_nowuser'] = null;
-
-// ============================================================================
-//  Pure utility helpers (no DB, no I/O)
-// ============================================================================
-
-function jiekoufunc_trans($data) {
-    $data = str_replace("]]>", "]]]]><![CDATA[>", $data);
-    return "<![CDATA[" . $data . "]]>";
-}
-
-function jiekoufunc_sanitize_xml($str) {
-    // Strip characters illegal in XML 1.0
-    $str = preg_replace('/[\\x00-\\x08\\x0B\\x0C\\x0E-\\x1F\\x7F]/', '', $str);
-    $str = str_replace("\xE2\x80\xA8", "", $str); // LINE SEPARATOR
-    $str = str_replace("\xE2\x80\xA9", "", $str); // PARAGRAPH SEPARATOR
-    return $str;
-}
-
-function jiekoufunc_islegal($num) {
-    return strlen(strval($num)) == 0 || is_numeric($num);
-}
-
-function jiekoufunc_packBool($bool) {
-    if ($bool) return "YES";
-    return "NO";
-}
-
-function jiekoufunc_comp($a, $b) {
-    if (intval($a[1]) > intval($b[1])) {
-        return -1;
-    } elseif (intval($a[1]) == intval($b[1])) {
-        return 0;
-    } else {
-        return 1;
-    }
-}
-
-function jiekoufunc_getdays($year, $month) {
-    $days = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-    if ($month != 2) return $days[$month - 1];
-    if ($year % 4 != 0) return 28;
-    if ($year % 100 == 0 && $year % 400 != 0) return 28;
-    return 29;
-}
 
 // ============================================================================
 //  DB helper functions
