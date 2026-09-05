a7565e83bf1cfe3d3f58f4ef3123174401cbc34c
陈少春 <116076400+ThywQuake@users.noreply.github.com>
2026-09-05 10:47:49 +0800

    build: 发布论坛图片加载优先级优化
    重新构建新论坛，提前加载附近图片、优先当前可见图片，并限制后台图片并发，为跳楼和图廊切换保留下载容量。

---
+++ forum/new-assets/list-DwM3o-Y3.js
@@ -0,0 +1,1 @@
+import{q as h}from"./index-D2cfJZTd.js";const t=[["path",{d:"M3 5h.01",key:"18ugdj"}],["path",{d:"M3 12h.01",key:"nlz23k"}],["path",{d:"M3 19h.01",key:"noohij"}],["path",{d:"M8 5h13",key:"1pao27"}],["path",{d:"M8 12h13",key:"1za7za"}],["path",{d:"M8 19h13",key:"m83p4d"}]],e=h("list",t);export{e as L};
